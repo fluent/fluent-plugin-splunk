00296eec993d5967cb62ba741b51e798489b130e
Yuki Ito <yuki@gnnk.net>
2017-03-01 19:06:31 +0900

    Initial commit

---
+++ lib/fluent/plugin/splunk.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/splunk/version"
+
+module Fluent
+  module Plugin
+    module Splunk
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/splunk/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Splunk
+      VERSION = "0.1.0"
+    end
+  end
+end
