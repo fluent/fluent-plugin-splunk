2d816ef19537c3a7dc845b552d1af6db136a8b91
Yuki Ito <yuki@gnnk.net>
2017-03-10 13:30:38 +0900

    Prepare test base

--- Rakefile
+++ Rakefile
@@ -1,2 +1,10 @@
-require "bundler/gem_tasks"
-task :default => :spec
+require 'bundler/gem_tasks'
+require 'rake/testtask'
+
+Rake::TestTask.new do |test|
+  test.libs << 'test'
+  test.pattern = 'test/test_*.rb'
+  test.verbose = true
+end
+
+task :default => :test
---
+++ test/test_out_splunk_tcp.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require 'fluent/test'
+
+class SplunkTCPOutputTest < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def create_driver(conf)
+  end
+
+  def test_configure
+  end
+
+  sub_test_case 'HTTP' do
+    test '' do
+    end
+  end
+
+  sub_test_case 'HTTPS' do
+    test '' do
+    end
+  end
+end
