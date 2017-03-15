1f4c731a1a124f5d32a82097f6a25fb991e0dca2
Yuki Ito <yuki@gnnk.net>
2017-03-15 15:33:58 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,19 +5,30 @@ set -e
 COMMAND=$1
 VERSION=$2
 REPO=450769122572.dkr.ecr.us-east-1.amazonaws.com/splunk-for-test:${VERSION}
+#REPO=splunk-for-test:${VERSION}
+
+PORTS="-p 8000:8000 -p 8089:8089 -p 8191:8191 -p 12300:12300 -p 1514:1514 -p 8088:8088 \
+       -p 8200:8000 -p 8289:8289 -p 8391:8391 -p 12500:12500 -p 1714:1714 -p 8288:8288"
 
 case "$COMMAND" in
   login)
-    $(aws ecr get-login --region us-east-1)
+    if [ -z "${CI:-}" ]; then
+      PROFILE="--profile engineering"
+    else
+      PROFILE=""
+    fi
+    $(aws $PROFILE ecr get-login --region us-east-1)
     ;;
   pull)
     docker pull ${REPO}
     ;;
   run)
-    docker run -d -p 8000:8000 -p 8089:8089 -p 8191:8191 -p 12300:12300 -p 1514:1514 -p 8088:8088 \
-                  -p 8200:8000 -p 8289:8289 -p 8391:8391 -p 12500:12500 -p 1714:1714 -p 8288:8288 ${REPO}
+    docker run -d ${PORTS} ${REPO}
+    ;;
+  debug_run)
+    docker run ${PORTS} ${REPO}
     ;;
   *)
-    wcho "Unkowon command"
+    echo "Unkowon command"
     ;;
 esac
