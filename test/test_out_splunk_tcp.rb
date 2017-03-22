5305414ca6f246709f9bbe751156580b6d8eb035
Yuki Ito <yuki@gnnk.net>
2017-03-22 15:16:03 +0900

    [TCP] Fix tests

--- test/test_out_splunk_tcp.rb
+++ test/test_out_splunk_tcp.rb
@@ -17,6 +17,7 @@ class SplunkTCPOutputTest < Test::Unit::TestCase
 
   CONFIG = %[
     port 8089
+    event_key event
   ]
 
   def create_driver(conf = CONFIG)
@@ -30,12 +31,18 @@ class SplunkTCPOutputTest < Test::Unit::TestCase
     }.configure(conf)
   end
 
-  def get_events(port, source)
-    query(port, {'search' => "search source=\"#{source}\""})
-  end
-
-  def cert_dir(file)
-    File.expand_path(File.join('../cert', file), __FILE__)
+  ## query(port, 'source="SourceName"')
+  def get_events(port, search_query, expected_num = 1)
+    retries = 0
+    events = []
+    while events.length != expected_num
+      print '-' unless retries == 0
+      sleep(3)
+      events = query(port, {'search' => 'search ' + search_query})
+      retries += 1
+      raise "exceed query retry limit" if retries > 20
+    end
+    events
   end
 
   def query(port, q)
@@ -53,8 +60,7 @@ class SplunkTCPOutputTest < Test::Unit::TestCase
     d = create_driver
     assert_equal 'localhost', d.instance.host
     assert_equal 8089, d.instance.port
-    assert_equal nil, d.instance.source
-    assert_equal 'time', d.instance.time_key
+    assert_equal 'event', d.instance.event_key
     assert_equal false, d.instance.ssl_verify_peer
     assert_equal nil, d.instance.ca_file
     assert_equal nil, d.instance.client_cert
@@ -66,11 +72,13 @@ class SplunkTCPOutputTest < Test::Unit::TestCase
   [{sub_test_case_name: 'TCP', query_port: 8089, server_port: 12300, config: %[
                                                                                host 127.0.0.1
                                                                                port 12300
+                                                                               event_key event
                                                                                ssl_verify_peer false
                                                                              ]},
    {sub_test_case_name: 'SSL', query_port: 8289, server_port: 12500, config: %[
                                                                                host 127.0.0.1
                                                                                port 12500
+                                                                               event_key event
                                                                                ssl_verify_peer true
                                                                                ca_file #{File.expand_path('../cert/cacert.pem', __FILE__)}
                                                                                client_cert #{File.expand_path('../cert/client.pem', __FILE__)}
@@ -79,19 +87,34 @@ class SplunkTCPOutputTest < Test::Unit::TestCase
   ].each do |test_config|
     sub_test_case test_config[:sub_test_case_name] do
       teardown do
-        query(test_config[:query_port], {'search' => 'search source="tcp:12300" | delete'})
+        query(test_config[:query_port], {'search' => "search source=\"tcp:#{test_config[:server_port]}\" | delete"})
       end
 
-      test 'emit' do
+      test 'single insert' do
         d = create_driver(test_config[:config])
-        event = {'test' => SecureRandom.hex}
-        time = Time.now.to_i
-        d.emit(event, time)
+        time = Time.now.to_i - 100
+        event = {'time' => time, 'test' => SecureRandom.hex}
+        d.emit({'event' => event.to_json}, time)
         d.run
-        sleep(3)
-        result = get_events(test_config[:query_port], "tcp:#{test_config[:server_port]}")[0]
+        result = get_events(test_config[:query_port], "source=\"tcp:#{test_config[:server_port]}\"")[0]
         assert_equal(time, result['result']['_time'].to_i)
-        assert_equal(event.merge({'time' => time}), JSON.parse(result['result']['_raw']))
+        assert_equal(event, JSON.parse(result['result']['_raw']))
+      end
+
+      test 'batched insert' do
+        d = create_driver(test_config[:config])
+        time0 = Time.now.to_i - 100
+        event0 = {'time' => time0, 'test' => SecureRandom.hex}
+        time1 = Time.now.to_i - 200
+        event1 = {'time' => time1, 'test' => SecureRandom.hex}
+        d.emit({'event' => event0.to_json}, time0)
+        d.emit({'event' => event1.to_json}, time1)
+        d.run
+        events = get_events(test_config[:query_port], "source=\"tcp:#{test_config[:server_port]}\"", 2)
+        assert_equal(time0, events[0]['result']['_time'].to_i)
+        assert_equal(event0, JSON.parse(events[0]['result']['_raw']))
+        assert_equal(time1, events[1]['result']['_time'].to_i)
+        assert_equal(event1, JSON.parse(events[1]['result']['_raw']))
       end
     end
   end
