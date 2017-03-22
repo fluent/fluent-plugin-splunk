54736f3a496b4bb0084a14c642367b8969422a70
Yuki Ito <yuki@gnnk.net>
2017-03-22 15:18:30 +0900

    Move sharable test helper functions to helper.rb

--- test/test_out_splunk_tcp.rb
+++ test/test_out_splunk_tcp.rb
@@ -1,3 +1,4 @@
+require 'helper'
 require 'test/unit'
 require 'fluent/test'
 require 'fluent/plugin/out_splunk_tcp'
@@ -32,30 +33,6 @@ class SplunkTCPOutputTest < Test::Unit::TestCase
   end
 
   ## query(port, 'source="SourceName"')
-  def get_events(port, search_query, expected_num = 1)
-    retries = 0
-    events = []
-    while events.length != expected_num
-      print '-' unless retries == 0
-      sleep(3)
-      events = query(port, {'search' => 'search ' + search_query})
-      retries += 1
-      raise "exceed query retry limit" if retries > 20
-    end
-    events
-  end
-
-  def query(port, q)
-    uri = URI.parse("https://127.0.0.1:#{port}/services/search/jobs/export")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    req = Net::HTTP::Post.new(uri.path)
-    req.basic_auth('admin', 'changeme')
-    req.set_form_data(q.merge({'output_mode' => 'json', 'time_format' => '%s'}))
-    http.request(req).body.split("\n").map{|line| JSON.parse(line)}.delete_if{|json| json['lastrow']}
-  end
-
   test 'configure' do
     d = create_driver
     assert_equal 'localhost', d.instance.host
