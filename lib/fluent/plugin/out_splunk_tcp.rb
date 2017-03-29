f80b0ac35875d867e063a6cf4623752e001d808f
Yuki Ito <yuki@gnnk.net>
2017-03-29 18:27:13 +0900

    [TCP] Add support default formatter for json and kv

--- lib/fluent/plugin/out_splunk_tcp.rb
+++ lib/fluent/plugin/out_splunk_tcp.rb
@@ -1,4 +1,6 @@
 require 'fluent/output'
+require 'fluent/formatter'
+require 'fluent/config/error'
 require 'socket'
 require 'openssl'
 require 'json'
@@ -12,10 +14,19 @@ module Fluent
     config_param :host, :string, default: 'localhost'
     config_param :port, :integer, required: true
 
-    config_param :event_key, :string, required: true
+    config_param :format, :string, default: 'raw'
+
+    # for raw format
+    config_param :event_key, :string, default: nil
+
+    # for json, kv format
+    config_param :use_fluentd_time, :bool, default: true
+    config_param :time_key, :string, default: 'time'
+    config_param :time_format, :string, default: 'unixtime'
+    config_param :localtime, :bool, default: false
+
     config_param :line_breaker, :string, default: "\n"
 
-    ## TODO: more detailed option?
     ## For SSL
     config_param :ssl_verify_peer, :bool, default: false
     config_param :ca_file, :string, default: nil
@@ -25,6 +36,36 @@ module Fluent
 
     def configure(conf)
       super
+
+      case @time_format
+      when 'unixtime'
+        @time_formatter = lambda {|time| time }
+      else
+        @timef = Fluent::TimeFormatter.new(@time_format, @localtime)
+        @time_formatter = lambda {|time| @timef.format(time) }
+      end
+
+      case @format
+      when 'json'
+        if @use_fluentd_time
+          @formatter = lambda {|time, record| insert_time_to_front(time, record).to_json }
+        else
+          @formatter = lambda {|_time, record| record.to_json }
+        end
+      when 'kv'
+        if @use_fluentd_time
+          @formatter = lambda {|time, record| format_kv(insert_time_to_front(time, record)) }
+        else
+          @formatter = lambda {|_time, record| format_kv(record) }
+        end
+      when 'raw'
+        unless @event_key
+          raise ConfigError, "'event_key' option is required for format 'raw'"
+        end
+        @formatter = lambda {|_time, record| record[@event_key] || '' }
+      else
+        raise ConfigError, "invalid 'format' option: #{@format}"
+      end
     end
 
     def start
@@ -39,8 +80,9 @@ module Fluent
       return if chunk.empty?
 
       payload = ''
-      chunk.msgpack_each do |_time, record|
-        payload << (record[@event_key] + @line_breaker)
+      chunk.msgpack_each do |time, record|
+        event = @formatter.call(time, record)
+        payload << (event + @line_breaker) unless event.empty?
       end
 
       unless payload.empty?
@@ -51,6 +93,26 @@ module Fluent
     end
 
     private
+    def insert_time_to_front(time, record)
+      record.delete(@time_key)
+      {@time_key => @time_formatter.call(time)}.merge(record)
+    end
+
+    def format_kv(record)
+      record.map{|k,v|
+        case v
+        when nil
+          "#{k}="
+        when Integer
+          "#{k}=#{v}"
+        when Float
+          "#{k}=#{v}"
+        else
+          "#{k}=\"#{v.to_s.gsub('"', '\"')}\""
+        end
+      }.join(' ')
+    end
+
     def create_socket
       @ssl_verify_peer ? create_ssl_socket : create_tcp_socket
     end
