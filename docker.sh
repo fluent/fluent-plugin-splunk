c02c43ec3d3b3f4615a83529a9248230805bff0e
Yuki Ito <yuki@gnnk.net>
2017-03-30 11:29:45 +0900

    [TCP] Add tests for format related options

--- docker.sh
+++ docker.sh
@@ -18,21 +18,26 @@ VERSION=$2
 IMAGE=450769122572.dkr.ecr.us-east-1.amazonaws.com/splunk-for-test:${VERSION}
 IMAGE_LOCAL=splunk-for-test:${VERSION}
 
-PORTS="-p 8000:8000 -p 8089:8089 -p 8191:8191 -p 12300:12300 -p 1514:1514 -p 8088:8088 \
-       -p 8200:8000 -p 8289:8289 -p 8391:8391 -p 12500:12500 -p 1714:1714 -p 8288:8288"
+PORTS="-p 8000:8000 -p 8089:8089 -p 8191:8191 -p 12300:12300 -p 12301:12301 -p 12302:12302 -p 12303:12303 -p 12304:12304 -p 12305:12305 -p 1514:1514 -p 8088:8088 \
+       -p 8200:8000 -p 8289:8289 -p 8391:8391 -p 12500:12500 -p 12501:12501 -p 12502:12502 -p 12503:12503 -p 12504:12504 -p 12505:12505 -p 1714:1714 -p 8288:8288"
+
+VOLUME="-v ${PWD}/test/config/props.conf:/opt/splunk_tcp/etc/system/local/props.conf \
+        -v ${PWD}/test/config/props.conf:/opt/splunk_ssl/etc/system/local/props.conf \
+        -v ${PWD}/test/config/inputs.tcp.conf:/opt/splunk_tcp/etc/apps/search/local/inputs.conf \
+        -v ${PWD}/test/config/inputs.ssl.conf:/opt/splunk_ssl/etc/apps/search/local/inputs.conf"
 
 case "$COMMAND" in
   pull)
     docker pull ${IMAGE}
     ;;
   run)
-    docker run -d ${PORTS} ${IMAGE}
+    docker run -d ${PORTS} ${VOLUME} ${IMAGE}
     ;;
   stop)
     docker stop $(docker ps -q --filter ancestor=${IMAGE})
     ;;
   debug_run)
-    docker run ${PORTS} ${IMAGE_LOCAL} --debug
+    docker run ${PORTS} ${VOLUME} ${IMAGE_LOCAL} --debug
     ;;
   build)
     docker build --no-cache=true -t ${IMAGE_LOCAL} test/Dockerfiles/enterprise/${VERSION}
--- test/test_out_splunk_tcp.rb
+++ test/test_out_splunk_tcp.rb
@@ -37,7 +37,13 @@ class SplunkTCPOutputTest < Test::Unit::TestCase
     d = create_driver
     assert_equal 'localhost', d.instance.host
     assert_equal 8089, d.instance.port
+    assert_equal 'raw', d.instance.format
     assert_equal 'event', d.instance.event_key
+    assert_equal true, d.instance.use_fluentd_time
+    assert_equal 'time', d.instance.time_key
+    assert_equal 'unixtime', d.instance.time_format
+    assert_equal false, d.instance.localtime
+    assert_equal "\n", d.instance.line_breaker
     assert_equal false, d.instance.ssl_verify_peer
     assert_equal nil, d.instance.ca_file
     assert_equal nil, d.instance.client_cert
@@ -45,53 +51,293 @@ class SplunkTCPOutputTest < Test::Unit::TestCase
     assert_equal nil, d.instance.client_key_pass
   end
 
+  def self.merge_config(config1, config2)
+    [config1, config2].join("\n")
+  end
+
+  def merge_config(config1, config2)
+    self.class.merge_config(config1, config2)
+  end
+
+
+  PORT_MAP = {
+    fluentd_json_unixtime: 0,
+    fluentd_json_unixtime2: 1,
+    fluentd_json_strftime: 2,
+    fluentd_kv_unixtime: 3,
+    fluentd_kv_unixtime2: 4,
+    fluentd_kv_strftime: 5,
+  }
+
+  def port(base, type = :fluentd_json_unixtime)
+    diff = PORT_MAP[type]
+    raise "invalid port type" unless diff
+    base + diff
+  end
+
+  def with_timezone(tz)
+    oldtz, ENV['TZ'] = ENV['TZ'], tz
+    yield
+  ensure
+    ENV['TZ'] = oldtz
+  end
+
+  ## It is assumed string elements doesn't contain ' ' and '='
+  def parse_kv(str)
+    str.split(' ').map{|attr|
+      k, v = attr.split('=')
+      if v.start_with?('"') && v.end_with?('"')
+        v = v.gsub(/^"/, '').gsub(/"$/, '').gsub('\"', '"')
+      elsif v =~ /^\d+$/
+        v = v.to_i
+      elsif /^\d+\.\d+$/
+        v = v.to_f
+      else
+        raise "invalid value as kv: #{v}"
+      end
+      [k, v]
+    }.to_h
+  end
+
   ## I just wanna run same test code for HTTP and HTTPS...
-  [{sub_test_case_name: 'TCP', query_port: 8089, server_port: 12300, config: %[
-                                                                               host 127.0.0.1
-                                                                               port 12300
-                                                                               event_key event
-                                                                               ssl_verify_peer false
-                                                                             ]},
-   {sub_test_case_name: 'SSL', query_port: 8289, server_port: 12500, config: %[
-                                                                               host 127.0.0.1
-                                                                               port 12500
-                                                                               event_key event
-                                                                               ssl_verify_peer true
-                                                                               ca_file #{File.expand_path('../cert/cacert.pem', __FILE__)}
-                                                                               client_cert #{File.expand_path('../cert/client.pem', __FILE__)}
-                                                                               client_key #{File.expand_path('../cert/client.key', __FILE__)}
-                                                                             ]}
+  [{sub_test_case_name: 'TCP', query_port: 8089, server_port_base: 12300, config: %[
+                                                                                    host 127.0.0.1
+                                                                                    ssl_verify_peer false
+                                                                                  ]},
+   {sub_test_case_name: 'SSL', query_port: 8289, server_port_base: 12500, config: %[
+                                                                                    host 127.0.0.1
+                                                                                    ssl_verify_peer true
+                                                                                    ca_file #{File.expand_path('../cert/cacert.pem', __FILE__)}
+                                                                                    client_cert #{File.expand_path('../cert/client.pem', __FILE__)}
+                                                                                    client_key #{File.expand_path('../cert/client.key', __FILE__)}
+                                                                                  ]}
   ].each do |test_config|
     sub_test_case test_config[:sub_test_case_name] do
       teardown do
-        query(test_config[:query_port], {'search' => "search source=\"tcp:#{test_config[:server_port]}\" | delete"})
+        PORT_MAP.keys.each do |port|
+          query(test_config[:query_port], {'search' => "search source=\"tcp:#{port(test_config[:server_port_base], port)}\" | delete"})
+        end
       end
 
-      test 'single insert' do
-        d = create_driver(test_config[:config])
-        time = Time.now.to_i - 100
-        event = {'time' => time, 'test' => SecureRandom.hex}
-        d.emit({'event' => event.to_json}, time)
-        d.run
-        result = get_events(test_config[:query_port], "source=\"tcp:#{test_config[:server_port]}\"")[0]
-        assert_equal(time, result['result']['_time'].to_i)
-        assert_equal(event, JSON.parse(result['result']['_raw']))
+      sub_test_case 'raw format' do
+        test 'single insert' do
+          config = merge_config(test_config[:config], %[
+            port #{port(test_config[:server_port_base])}
+            format raw
+            event_key event
+          ])
+          d = create_driver(config)
+          time = Time.now.to_i - 100
+          event = {'time' => time, 'test' => SecureRandom.hex}
+          d.emit({'event' => event.to_json}, time)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port(test_config[:server_port_base])}\"")[0]
+          assert_equal(time, result['result']['_time'].to_i)
+          assert_equal(event, JSON.parse(result['result']['_raw']))
+        end
+
+        test 'batched insert' do
+          config = merge_config(test_config[:config], %[
+            port #{port(test_config[:server_port_base])}
+            format raw
+            event_key event
+          ])
+          d = create_driver(config)
+          time0 = Time.now.to_i - 100
+          event0 = {'time' => time0, 'test' => SecureRandom.hex}
+          time1 = Time.now.to_i - 200
+          event1 = {'time' => time1, 'test' => SecureRandom.hex}
+          d.emit({'event' => event0.to_json}, time0)
+          d.emit({'event' => event1.to_json}, time1)
+          d.run
+          events = get_events(test_config[:query_port], "source=\"tcp:#{port(test_config[:server_port_base])}\"", 2)
+          assert_equal(time0, events[0]['result']['_time'].to_i)
+          assert_equal(event0, JSON.parse(events[0]['result']['_raw']))
+          assert_equal(time1, events[1]['result']['_time'].to_i)
+          assert_equal(event1, JSON.parse(events[1]['result']['_raw']))
+        end
       end
 
-      test 'batched insert' do
-        d = create_driver(test_config[:config])
-        time0 = Time.now.to_i - 100
-        event0 = {'time' => time0, 'test' => SecureRandom.hex}
-        time1 = Time.now.to_i - 200
-        event1 = {'time' => time1, 'test' => SecureRandom.hex}
-        d.emit({'event' => event0.to_json}, time0)
-        d.emit({'event' => event1.to_json}, time1)
-        d.run
-        events = get_events(test_config[:query_port], "source=\"tcp:#{test_config[:server_port]}\"", 2)
-        assert_equal(time0, events[0]['result']['_time'].to_i)
-        assert_equal(event0, JSON.parse(events[0]['result']['_raw']))
-        assert_equal(time1, events[1]['result']['_time'].to_i)
-        assert_equal(event1, JSON.parse(events[1]['result']['_raw']))
+      sub_test_case 'json format' do
+        test 'default' do
+          port = port(test_config[:server_port_base], :fluentd_json_unixtime)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format json
+          ])
+          d = create_driver(config)
+          time = Time.now.to_i - 100
+          event = {'test' => SecureRandom.hex}
+          d.emit(event, time)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+          assert_equal(time, result['result']['_time'].to_i)
+          assert_equal({'time' => time}.merge(event), JSON.parse(result['result']['_raw']))
+        end
+
+        test 'time_key=time2' do
+          port = port(test_config[:server_port_base], :fluentd_json_unixtime2)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format json
+            time_key time2
+          ])
+          d = create_driver(config)
+          time = Time.now.to_i - 100
+          event = {'test' => SecureRandom.hex}
+          d.emit(event, time)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+          assert_equal(time, result['result']['_time'].to_i)
+          assert_equal({'time2' => time}.merge(event), JSON.parse(result['result']['_raw']))
+        end
+
+        test 'time_key=strftime' do
+          port = port(test_config[:server_port_base], :fluentd_json_strftime)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format json
+            time_format %Y-%m-%dT%H:%M:%S%z
+          ])
+          d = create_driver(config)
+          time = Time.now.to_i - 100
+          event = {'test' => SecureRandom.hex}
+          d.emit(event, time)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+          assert_equal(time, result['result']['_time'].to_i)
+          assert_equal({'time' => Time.at(time).utc.strftime('%Y-%m-%dT%H:%M:%S%z')}.merge(event), JSON.parse(result['result']['_raw']))
+        end
+
+        test 'time_key=strftime, localtime=true' do
+          port = port(test_config[:server_port_base], :fluentd_json_strftime)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format json
+            time_format %Y-%m-%dT%H:%M:%S%z
+            localtime true
+          ])
+          time = Time.now.to_i - 100
+          with_timezone("UTC-04") do
+            d = create_driver(config)
+            event = {'test' => SecureRandom.hex}
+            d.emit(event, time)
+            d.run
+            result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+            assert_equal(time, result['result']['_time'].to_i)
+            assert_equal({'time' => Time.at(time).strftime('%Y-%m-%dT%H:%M:%S%z')}.merge(event), JSON.parse(result['result']['_raw']))
+            assert_equal(time, DateTime.strptime(JSON.parse(result['result']['_raw'])['time'], '%Y-%m-%dT%H:%M:%S%z').to_time.to_i)
+          end
+        end
+
+        test 'use_fluentd_time=false' do
+          port = port(test_config[:server_port_base], :fluentd_json_unixtime)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format json
+            use_fluentd_time false
+          ])
+          d = create_driver(config)
+          time0 = Time.now.to_i - 100
+          time1 = time0 - 100
+          event = {'time' => time0, 'test' => SecureRandom.hex}
+          d.emit(event, time1)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+          assert_equal(time0, result['result']['_time'].to_i)
+          assert_equal(event, JSON.parse(result['result']['_raw']))
+        end
+      end
+
+      sub_test_case 'kv format' do
+        test 'default' do
+          port = port(test_config[:server_port_base], :fluentd_kv_unixtime)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format kv
+          ])
+          d = create_driver(config)
+          time = Time.now.to_i - 100
+          event = {'test' => SecureRandom.hex, 'escape' => 'a"b'}
+          d.emit(event, time)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+          assert_equal(time, result['result']['_time'].to_i)
+          assert_equal({'time' => time}.merge(event), parse_kv(result['result']['_raw']))
+        end
+
+        test 'time_key=time2' do
+          port = port(test_config[:server_port_base], :fluentd_kv_unixtime2)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format kv
+            time_key time2
+          ])
+          d = create_driver(config)
+          time = Time.now.to_i - 100
+          event = {'test' => SecureRandom.hex}
+          d.emit(event, time)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+          assert_equal(time, result['result']['_time'].to_i)
+          assert_equal({'time2' => time}.merge(event), parse_kv(result['result']['_raw']))
+        end
+
+        test 'time_key=strftime' do
+          port = port(test_config[:server_port_base], :fluentd_kv_strftime)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format kv
+            time_format %Y-%m-%dT%H:%M:%S%z
+          ])
+          d = create_driver(config)
+          time = Time.now.to_i - 100
+          event = {'test' => SecureRandom.hex}
+          d.emit(event, time)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+          assert_equal(time, result['result']['_time'].to_i)
+          assert_equal({'time' => Time.at(time).utc.strftime('%Y-%m-%dT%H:%M:%S%z')}.merge(event), parse_kv(result['result']['_raw']))
+        end
+
+        test 'time_key=strftime, localtime=true' do
+          port = port(test_config[:server_port_base], :fluentd_kv_strftime)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format kv
+            time_format %Y-%m-%dT%H:%M:%S%z
+            localtime true
+          ])
+          time = Time.now.to_i - 100
+          with_timezone("UTC-04") do
+            d = create_driver(config)
+            event = {'test' => SecureRandom.hex}
+            d.emit(event, time)
+            d.run
+            result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+            assert_equal(time, result['result']['_time'].to_i)
+            assert_equal({'time' => Time.at(time).strftime('%Y-%m-%dT%H:%M:%S%z')}.merge(event), parse_kv(result['result']['_raw']))
+            assert_equal(time, DateTime.strptime(parse_kv(result['result']['_raw'])['time'], '%Y-%m-%dT%H:%M:%S%z').to_time.to_i)
+          end
+        end
+
+        test 'use_fluentd_time=false' do
+          port = port(test_config[:server_port_base], :fluentd_kv_unixtime)
+          config = merge_config(test_config[:config], %[
+            port #{port}
+            format kv
+            use_fluentd_time false
+          ])
+          d = create_driver(config)
+          time0 = Time.now.to_i - 100
+          time1 = time0 - 100
+          event = {'time' => time0, 'test' => SecureRandom.hex}
+          d.emit(event, time1)
+          d.run
+          result = get_events(test_config[:query_port], "source=\"tcp:#{port}\"")[0]
+          assert_equal(time0, result['result']['_time'].to_i)
+          assert_equal(event, parse_kv(result['result']['_raw']))
+        end
       end
     end
   end
