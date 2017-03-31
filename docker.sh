ea85bd8ec46e7dea026c8913e7ed5ca07ab9685d
Yuki Ito <yuki@gnnk.net>
2017-03-31 10:34:30 +0900

    Fix docker port forwarding for ssl web

--- docker.sh
+++ docker.sh
@@ -19,7 +19,7 @@ IMAGE=450769122572.dkr.ecr.us-east-1.amazonaws.com/splunk-for-test:${VERSION}
 IMAGE_LOCAL=splunk-for-test:${VERSION}
 
 PORTS="-p 8000:8000 -p 8089:8089 -p 8191:8191 -p 12300:12300 -p 12301:12301 -p 12302:12302 -p 12303:12303 -p 12304:12304 -p 12305:12305 -p 1514:1514 -p 8088:8088 \
-       -p 8200:8000 -p 8289:8289 -p 8391:8391 -p 12500:12500 -p 12501:12501 -p 12502:12502 -p 12503:12503 -p 12504:12504 -p 12505:12505 -p 1714:1714 -p 8288:8288"
+       -p 8200:8200 -p 8289:8289 -p 8391:8391 -p 12500:12500 -p 12501:12501 -p 12502:12502 -p 12503:12503 -p 12504:12504 -p 12505:12505 -p 1714:1714 -p 8288:8288"
 
 VOLUME="-v ${PWD}/test/config/props.conf:/opt/splunk_tcp/etc/system/local/props.conf \
         -v ${PWD}/test/config/props.conf:/opt/splunk_ssl/etc/system/local/props.conf \
